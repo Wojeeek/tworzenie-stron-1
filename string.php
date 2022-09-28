dcc7dc892fec3def757df33dd98b477e12153f79
dt19biskupw <wojtekbiskup6@gmail.com>
2022-09-28 14:26:46 +0200

    message1

--- string.php
+++ string.php
@@ -12,15 +12,19 @@
 <?php
 
 define("DOBRA_RADA","Bez chleba to sie nie najesz.");
-echo DOBRA_RADA. '<BR>';
+echo DOBRA_RADA.'<br>';
 echo strlen(DOBRA_RADA). '<br>';
-echo str_word_count(DOBRA_RADA) . '<br>';
-echo strpos(DOBRA_RADA) . '<br>';
+echo str_word_count(DOBRA_RADA). '<br>';
+echo strrev(DOBRA_RADA). '<br>';
+echo strpos(DOBRA_RADA ,'to'). '<br>';
 echo str_replace('chleba', 'mięsa', DOBRA_RADA). '<br>';
 echo strtolower(DOBRA_RADA). '<br>';
 echo strtoupper(DOBRA_RADA). '<br>';
 echo ucwords(DOBRA_RADA). '<br>';
-f
+echo trim(DOBRA_RADA). '<br>';
+echo strstr(DOBRA_RADA, 'nie'). '<br>';
+echo substr(DOBRA_RADA, 10, 5). '<br>';
+echo str_pad(DOBRA_RADA, 37, '-_-', STR_PAD_BOTH). '<br>';
 ?>
 </body>
 </html>
