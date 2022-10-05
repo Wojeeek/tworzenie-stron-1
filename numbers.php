23e09b99b117d4b55b25ad0a639e9b66cdd7f7fd
dt19biskupw <wojtekbiskup6@gmail.com>
2022-10-05 14:40:47 +0200

    message1

---
+++ numbers.php
@@ -0,0 +1,66 @@
+<!Doctype html>
+<html>
+</html>
+<?php
+$a=6;
+$b=4;
+$c=3.5;
+$d=2.6;
+$dodaj="59.85" + 100;
+echo "zmienna a na wartosc {$a} a funkcja is_int zwraca dla niej wynik";
+var_dump(is_int($a)) ; echo "<br>";
+echo "zmienna a na wartosc {$a} a funkcja is_int zwraca dla niej wynik";
+var_dump(is_float($a)); echo "<br>";
+echo "zmienna a na wartosc {$b} a funkcja is_int zwraca dla niej wynik";
+var_dump(is_float($b)) ; echo "<br>";
+echo "zmienna a na wartosc {$b} a funkcja is_int zwraca dla niej wynik";
+var_dump(is_float($b)) ; echo "<br>";
+echo "zmienna a na wartosc {$c} a funkcja is_int zwraca dla niej wynik";
+var_dump(is_int($c)) ; echo "<br>";
+echo "zmienna a na wartosc {$c} a funkcja is_int zwraca dla niej wynik";
+var_dump(is_float($c)); echo "<br>";
+echo "zmienna a na wartosc {$d} a funkcja is_int zwraca dla niej wynik";
+var_dump(is_float($d)); echo "<br>";
+echo "zmienna a na wartosc {$d} a funkcja is_int zwraca dla niej wynik";
+var_dump(is_int($d)) ; echo "<br>";
+echo "zmienna dodaj na wartość {$dodaj} a funkcje is_numeric zwraca dla niej wynik";
+var_dump(is_int($dodaj)) ; echo "<br>";
+echo "stała PHP_INT_MAX ma wartość ";
+var_dump(PHP_INT_MAX) ; echo "<br>";
+echo "stała PHP_INT_MIN ma wartość ";
+var_dump(PHP_INT_MIN); echo "<br>";
+echo "stała PHP_INT_SIZE ma wartość ";
+var_dump(PHP_INT_SIZE); echo "<br>";
+echo "stała PHP_FLOAT_MAX ma wartość ";
+var_dump(PHP_FLOAT_MAX); echo "<br>";
+echo "stała PHP_FLOAT_MIN ma wartość ";
+var_dump(PHP_FLOAT_MIN); echo "<br>";
+echo "stała PHP_FLOAT_DIG ma wartość ";
+var_dump(PHP_FLOAT_DIG); echo "<br>";
+echo "stała PHP_FLOAT_EPSILON ma wartość ";
+var_dump(PHP_FLOAT_EPSILON); echo "<br>";
+
+$x=5;
+$y=15;
+$suma =$x+$y;
+echo "wynik dodawania $x i $y wynosi $suma ". "<br>";
+$suma=$x-$y;
+echo "wynik odejmowania $x i $y wynosi $suma ". "<br>";
+$suma=$x/$y;
+echo "wynik dzielenia $x i $y wynosi $suma ". "<br>";
+$suma=$x*$y;
+echo "wynik mnożenia $x i $y wynosi $suma". "<br>";
+ var_dump(abs -4))?></p>
+echo min([0 ,150 ,30 ,20 ,-8 ,-200]). "<br>";
+echo max(0 , 150 ,  )."<br>";
+echo pi(). "<br>";
+echo round(3,43,65,). "<br>";
+echo rand(900, 1000). "<br>";
+echo sqrt([0, 64, 256, 65536]). "<br>";
+
+
+?>
+</html>
+
+
+
