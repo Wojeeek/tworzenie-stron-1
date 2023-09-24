d97a0160ee2d8b2cb81159844b6f75c748445655
Wojeeek <114578211+Wojeeek@users.noreply.github.com>
2023-09-24 21:49:09 +0200

    Update usun.php

--- bibliotekawb/pages/usun.php
+++ bibliotekawb/pages/usun.php
@@ -1,13 +1,13 @@
 <h1>Usuń dane tabeli działy</h1>
 
 <?php
-$Id_dzial = $_GET['id'];
+$Id_dzial = $_GET['id_thing'];
 $Nazwa = $_GET['Nazwa'];
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
 
     $query = "DELETE FROM dzialy WHERE `dzialy`.`Id_dzial` = '$Id_dzial'";
-    $result = mysqli_query($mysqliProceduralConection, $query);
+    $result = mysqli_query($conn, $query);
     if ($result) {
         echo "Poprawnie usunięto rekord";
         echo '<br><a href="?PAGE=dzialy">Pow    rót</a>';
@@ -16,7 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     }
 } else {
 
-    echo '<form action="?PAGE=usun&id='.$Id_dzial.'" method="post">
+    echo '<form action="?PAGE=usun&id_thing='.$Id_dzial.'" method="post">
  <p>Czy napewno chcesz usunąć ten rekord? <b>(NIE MOŻESZ TEGO ODWRÓCIĆ!)</b></p>
  <table>
                 <tr><td>Id_dzial</td><td><input type="text" name="Id_dzial" value="'.$Id_dzial.'" disabled </td></tr>
